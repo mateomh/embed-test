a2231fe47afc56b61a7d0b8be70c516d916fce58
mateomh <mmojicah.84@gmail.com>
2021-03-04 08:17:01 -0500

    Debug click config

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const clickConfig = () => {
   console.log(svcMca.tlb.api.getConfiguration);
   console.log('After');
   const scripts = document.getElementsByTagName('script');
-  console.log(scripts[0]);
+  console.log(scripts[0].innerHTML);
   // svcMca.tlb.api.getConfiguration('ALL', function (response) {
   //   console.log(response);
   //   if (response.result == 'success') {
