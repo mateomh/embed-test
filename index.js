bfce429b446a7774beded582911ed4b8b6115974
mateomh <mmojicah.84@gmail.com>
2021-03-05 11:03:56 -0500

    Debug click config

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const clickConfig = () => {
   // svcMca.tlb.api.readyForOperation();
   // svcMca.tlb.api.getConfiguration(ALL, null);
   console.log('New config');
-  alert('from the iframe14');
+  alert('from the iframe15');
   const callback = () => {console.log('hola')};
   const type ='ALL'
   console.log(window.globalThis);
