b78bd2f7ac09effeecbb71e228154338fe8abf96
mateomh <mmojicah.84@gmail.com>
2021-03-11 11:41:14 -0500

    Change config type parameter to be the first paramater

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const clickConfig = () => {
   console.log('New config');
   const type ='ALL'
   // svcMca.tlb.api.getConfiguration(type, callback);
-  svcMca.tlb.api.getConfiguration(function (configType, response) {
+  svcMca.tlb.api.getConfiguration('ALL', (configType, response) => {
     console.log('Config Type', configType);
 
     if (configType.result == 'success') 
@@ -108,7 +108,7 @@ const clickConfig = () => {
     {
       alert('Could not getConfiguration. finished with error: ' + configType.error);
     }
-  },'ALL');
+  });
   // console.log(resp);
   console.log('End config');
 };
