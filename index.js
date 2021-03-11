736ef54f062e106223571828dfa71ea73dd8b90f
mateomh <mmojicah.84@gmail.com>
2021-03-11 16:33:22 -0500

    Fix copy and paste error

--- index.js
+++ index.js
@@ -250,7 +250,7 @@ const clickStartComm = () => {
   var inData = {};
   inData.SVCMCA_ANI = '5551234';
   inData.SVCMCA_CONTACT_ID = '1234567890';
-  svcMca.tlb.api.newCommEvent('PHONE', 'ORA_SERVICE', '12345-1234-67890', inData, null, function (response) {
+  svcMca.tlb.api.startCommEvent('PHONE', 'ORA_SERVICE', '12345-1234-67890', inData, null, function (response) {
     if (response.result == 'success') {
       console.log('COMM RESPONSE: ',response);
       console.log('Customer: '+response.outData.SVCMCA_CONTACT_NAME +' ('+response.outData.SVCMCA_CONTACT_ID +')');
