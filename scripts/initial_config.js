1bdb461ff3f4e1175a23523130417c2e3d507431
mateomh <mmojicah.84@gmail.com>
2021-05-11 14:57:57 -0500

    Add string options for the alert

--- scripts/initial_config.js
+++ scripts/initial_config.js
@@ -112,7 +112,7 @@ const registerListeners = () => {
   // ON DATA UPDATED
   svcMca.tlb.api.onDataUpdated(channel,classification, (resp) => {
     console.log('THIS IS THE DATA UPDATED CALLBACK', resp);
-    alert ('Wrapup Info \n', JSON.stringify(resp, null, 2));
+    alert ('Wrapup Info \n', JSON.stringify(resp.outData));
   }, channelType);
   // ON OUTGOING EVENT
   svcMca.tlb.api.onOutgoingEvent(channel,classification, (resp) => {
