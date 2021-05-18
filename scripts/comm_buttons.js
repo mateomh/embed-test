e01c2c9045bbeeebf2fc4658c64ed58bd061bcc8
mateomh <mmojicah.84@gmail.com>
2021-05-18 14:48:54 -0500

    debgging fatoken

--- scripts/comm_buttons.js
+++ scripts/comm_buttons.js
@@ -1,7 +1,6 @@
 const urlProspects = 'https://ccur-dev1.fa.us6.oraclecloud.com/crmRestApi/resources/latest/contacts';
 const urlLeads = 'https://ccur-dev1.fa.us6.oraclecloud.com/crmRestApi/resources/latest/leads/';
 
-
 const userInfo = {
   "Type": "ZCA_PROSPECT",
   "Country": "CR",
